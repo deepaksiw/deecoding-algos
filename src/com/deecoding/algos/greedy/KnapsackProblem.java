6b0695ccff292d8cfaea7d4a2ab2caa506e8a12e
Deepak Pandey <deepaksiw@gmail.com>
2021-11-23 10:00:58 +0530

    To test the repo

--- src/com/deecoding/algos/greedy/KnapsackProblem.java
+++ src/com/deecoding/algos/greedy/KnapsackProblem.java
@@ -3,7 +3,7 @@ package com.deecoding.algos.greedy;
 public class KnapsackProblem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is Kanapsack algo...");
 
 	}
 
